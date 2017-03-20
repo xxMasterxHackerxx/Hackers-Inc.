b4357f028b500e2724f46ca98c5a307583265a78
Joseph Beaver <jbeaver7065@vbtc.net>
2017-03-20 13:31:19 -0400

    step e

--- App0301/App0301/App0301.cpp
+++ App0301/App0301/App0301.cpp
@@ -1,14 +1,33 @@
-// App0301.cpp : Defines the entry point for the console application.
-//
 
 #include <iostream>
+#include <string>
+#include <time.h>
 using namespace std;
 
-
+void play(const string& aWord);
 int main()
 {
 	cout << "Hello World";
 	system("pause");
 	return 0;
 }
+void play(const string& aWord){
+	string guess, secretWord;
+	do{
 
+		cout << "Enter your guess: ";
+		cin >> guess;
+
+		if ((guess != aWord) && (guess != "quit")){
+			cout << " Your are Very Very Wrong!!" << endl;
+		}
+
+
+	} while ((guess != aWord) && (guess != "quit"));
+	if (guess == aWord){
+		cout << "You are Correct!!!!!, Thanks for playing" << endl;
+	}
+	if (guess == "quit"){
+		cout << "Thanks for playing" << endl;
+	}
+}
