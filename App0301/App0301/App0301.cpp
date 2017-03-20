f300461a9bb6aa23984a4dcd92d206ab7062ed56
Anakin Hanley <ahanley7401@vbtc.net>
2017-03-20 13:06:05 -0400

    App0301

---
+++ App0301/App0301/App0301.cpp
@@ -0,0 +1,14 @@
+// App0301.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello World";
+	system("pause");
+	return 0;
+}
+
