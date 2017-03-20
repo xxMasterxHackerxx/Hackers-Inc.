713606f90dd98502674cdc84442840ccffeb1ac5
Anakin Hanley <ahanley7401@vbtc.net>
2017-03-20 13:18:17 -0400

    Steps a and b

--- App0301/App0301/App0301.cpp
+++ App0301/App0301/App0301.cpp
@@ -2,13 +2,35 @@
 //
 
 #include <iostream>
+#include <string>
+#include <time.h>
 using namespace std;
+
+string randomWord();
+string mixUp(const string& aWord);
+void welcomePlayer(const string& aJumble);
+void play(const string& aWord);
 
 
 int main()
 {
-	cout << "Hello World";
+	string someRandomWord = randomWord();
+	string jumble = mixUp(someRandomWord);
+	welcomePlayer(jumble);
+	play(someRandomWord);
+
 	system("pause");
 	return 0;
 }
+
+string randomWord()
+{
+	const int NUM_WORDS = 5;
+	const string SECRET_WORDS[NUM_WORDS] = {
+		"edgelord", "cpu", "hack", "dank", "thelegend"
+	};
+	srand(time(NULL));
+	int ran = rand() % NUM_WORDS;
+	string secretWord = SECRET_WORDS[ran];
+	return secretWord;
 
